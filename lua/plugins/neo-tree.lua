4780e757641924c2ef2ab5f252370840a7b03c27
patapan <alexpatapan@gmail.com>
2025-04-22 19:24:53 +1000

    Add fuzzy finder (tg + tf) to neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,21 +1,43 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  -- put it after the core LazyVim neotree spec (optional, usually fine)
-  -- priority = 1001,
 
-  ---@param opts table the default opts passed in by LazyVim
+  ---@param opts table
   opts = function(_, opts)
-    -- make sure the nested tables exist
     opts.filesystem = opts.filesystem or {}
     opts.filesystem.window = opts.filesystem.window or {}
+
+    -- 1.  key‑mappings (merge into whatever’s already there)
     opts.filesystem.window.mappings = vim.tbl_extend("force",
       opts.filesystem.window.mappings or {},
       {
-        ["/"] = "noop",           -- let `/` fall through to normal Vim search
-        ["#"] = "fuzzy_finder",   -- move fuzzy finder here
+        ["/"] = "noop",            -- regular Vim search
+        ["#"] = "fuzzy_finder",    -- fuzzy finder
         ["D"] = "fuzzy_finder_directory",
+        ["tg"] = "telescope_grep", -- NEW → live_grep under the node
+        ["tf"] = "telescope_find", -- NEW → find_files under the node
       }
     )
+
+    -- 2. custom commands those keys trigger
+    opts.commands = vim.tbl_extend("force", opts.commands or {}, {
+      telescope_grep = function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()                      -- full path of cursor node
+        require("telescope.builtin").live_grep({
+          cwd = path,
+          search_dirs = { path },
+        })
+      end,
+
+      telescope_find = function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        require("telescope.builtin").find_files({
+          cwd = path,
+          search_dirs = { path },
+        })
+      end,
+    })
   end,
 }
 
